5f76e600947905fc1200f3a8415073f4682ae91f
maximiliano745 <maximiliano745@gmail.com>
2023-09-05 02:11:30 -0300

    ws url??

--- src/componentes/paginas/Chat/api/index.js
+++ src/componentes/paginas/Chat/api/index.js
@@ -1,9 +1,10 @@
 //var socket = new WebSocket('ws://localhost:8080/ws');  // Hera 9000!!!!
-const API_URL = "https://geochat-efn9.onrender.com/"
 
-const socket = new WebSocket("wss://" + API_URL + "/api/v2/users/ws");
+var socket = new WebSocket('wss://https://geochat-efn9.onrender.com/api/v2/users/ws');
 
-//var socket = new WebSocket('ws://'+API_URL+''+'wss:/api/v2/users/ws');
+//const API_URL = "https://geochat-efn9.onrender.com/"
+//const socket = new WebSocket("wss://" + API_URL + "/api/v2/users/ws");
+
 
 let connect = (cb) => {
     console.log("Conectando....");
