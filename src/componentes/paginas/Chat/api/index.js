944ef87ff2ea3fdb3ec64078dba44bfeb41a8031
maximiliano745 <maximiliano745@gmail.com>
2023-09-04 23:50:44 -0300

    Chat e Imail

--- src/componentes/paginas/Chat/api/index.js
+++ src/componentes/paginas/Chat/api/index.js
@@ -1,4 +1,5 @@
-var socket = new WebSocket('ws://localhost:8080/ws');  // Hera 9000!!!!
+//var socket = new WebSocket('ws://localhost:8080/ws');  // Hera 9000!!!!
+var socket = new WebSocket('ws://https://geochat-efn9.onrender.com/ws');
 
 let connect = (cb) => {
     console.log("Conectando....");
