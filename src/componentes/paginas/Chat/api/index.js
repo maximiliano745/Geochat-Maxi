9d41aafb273cf39df5f3f6115cbf88840cf73b07
maximiliano745 <maximiliano745@gmail.com>
2023-10-24 17:17:44 -0300

    Guardando Grupos2

--- src/componentes/paginas/Chat/api/index.js
+++ src/componentes/paginas/Chat/api/index.js
@@ -1,4 +1,4 @@
-//const API_URL = "geochat-efn9.onrender.com"
+const API_URL = "geochat-efn9.onrender.com"
 
 //var socket = new WebSocket("ws://localhost:10000/api/v2/users/ws");
 var socket = new WebSocket("wss://" + API_URL + "/api/v2/users/wss");
